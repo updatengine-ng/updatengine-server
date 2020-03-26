4a64885d5c099d56894e365f69dd1782f1609404
Noël Martinon <noel.martinon@gmail.com>
2020-03-26 11:35:09 -0400

    Add lines to fix potential pip3 mysqlclient issue

--- updatengine/__init__.py
+++ updatengine/__init__.py
@@ -1,1 +1,10 @@
+#
+# Uncomment lines below in case of error "Did you install mysqlclient or MySQL-python ?" :
+# and run ${INST_DIR}/bin/pip3 install pymysql
+#
+'''
+import pymysql
 
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+'''
