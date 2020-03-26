49a809630ab3e56090e711331f4d04b567c54bca
Noël Martinon <noel.martinon@gmail.com>
2020-03-26 11:30:03 -0400

    Fix version check

--- updatengine/utils.py
+++ updatengine/utils.py
@@ -1,10 +1,10 @@
 import urllib.request, urllib.error, urllib.parse
 import re
-    
+
 def extract_text_from_url(url, regex):
     try:
         req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-        html = urllib.request.urlopen(req).read()
+        html = urllib.request.urlopen(req).read().decode("utf-8")
         retval = re.search(regex, html, re.IGNORECASE | re.DOTALL).group(1)
     except:
         retval = ''
