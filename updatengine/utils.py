1b71008834184f90377f3852714938e87b2a46bb
Xylle <xylle409@gmail.com>
2022-03-20 07:58:56 +0100

    Update utils.py

--- updatengine/utils.py
+++ updatengine/utils.py
@@ -1,5 +1,6 @@
 import urllib.request, urllib.error, urllib.parse
 import re
+import json
 
 def extract_text_from_url(url, regex):
     try:
@@ -8,5 +9,16 @@ def extract_text_from_url(url, regex):
         retval = re.search(regex, html, re.IGNORECASE | re.DOTALL).group(1)
     except:
         retval = ''
+    return retval
+
+def get_latest_release_version(url):
+    try:
+        headers = {'User-Agent': "Magic Browser", 'Accept': "application/vnd.github.v3+json"}
+        req = urllib.request.Request(url, headers= headers)
+        html = urllib.request.urlopen(req).read().decode("utf-8")
+        releases = json.loads(html)
+        retval = releases[0]['tag_name']
+    except Exception as e:
+        retval = str(e)                                                                                                                                                                                                                       
     return retval
 
