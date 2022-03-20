f54751e94fa128132a1837988a8fea504fc37e5b
Xylle <xylle409@gmail.com>
2022-03-20 08:00:05 +0100

    Update views.py

--- updatengine/views.py
+++ updatengine/views.py
@@ -10,8 +10,6 @@ def check_version(request):
         json_file = os.path.join(os.path.dirname(__file__), 'static/json/app.json')
         json_data = open(json_file).read()
         json_list = json.loads(json_data)
-        # regex = 'releases/tag/(.+?)">'
-        # version = extract_text_from_url(url, regex)
         repository = json_list['repository']
         url_release = 'https://api.github.com/repos/' + repository + '/releases'
         version = get_latest_release_version(url_release)
