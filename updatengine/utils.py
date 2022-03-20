0d5745160bb25bb2ce855691eee334fe67232bb7
Xylle <xylle409@gmail.com>
2022-03-20 08:00:54 +0100

    Update utils.py

--- updatengine/utils.py
+++ updatengine/utils.py
@@ -2,15 +2,6 @@ import urllib.request, urllib.error, urllib.parse
 import re
 import json
 
-def extract_text_from_url(url, regex):
-    try:
-        req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-        html = urllib.request.urlopen(req).read().decode("utf-8")
-        retval = re.search(regex, html, re.IGNORECASE | re.DOTALL).group(1)
-    except:
-        retval = ''
-    return retval
-
 def get_latest_release_version(url):
     try:
         headers = {'User-Agent': "Magic Browser", 'Accept': "application/vnd.github.v3+json"}
