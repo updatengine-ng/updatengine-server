d49b9c19de8e385330128c296b25ce83a78f35b5
noelmartinon <noel.martinon@gmail.com>
2023-07-20 18:52:11 -0400

    Add docker installation

---
+++ install/docker/README.md
@@ -0,0 +1,5 @@
+# Docker installation
+
+git clone https://github.com/updatengine-ng/updatengine-server
+cd updatengine-server/install/docker
+./install.sh
---
+++ install/docker/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# Defaults SECRET_KEY to a random value
+SECRET_KEY_FILE=./secret_key
+if [ -z $SECRET_KEY ]; then
+    if [ ! -f $SECRET_KEY_FILE ]; then
+        echo "Generate a secret key"
+        dd bs=48 count=1 if=/dev/urandom 2>/dev/null | base64 > $SECRET_KEY_FILE
+        chmod go-r $SECRET_KEY_FILE
+    fi
+    export SECRET_KEY=`cat $SECRET_KEY_FILE`
+else
+    echo $SECRET_KEY > $SECRET_KEY_FILE
+fi
+
+# Create settings.py using environment settings
+echo "Create settings.py"
+envsubst < ./install/docker/settings.py.in > ./updatengine/settings.py
+
+# If settings_local.py exists, add it
+if [ -f ./install/docker/updatengine/settings_local.py ]; then
+    cp ./install/docker/updatengine/settings_local.py ./updatengine/settings_local.py
+fi
+
+# Collect static files
+echo "Collect static files"
+python manage.py collectstatic --clear --noinput --verbosity=0
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate --noinput
+
+# load initial data if needed
+echo 'select count(*) from configuration_globalconfig;' | python manage.py dbshell | grep -v 'count' | grep 0 > /dev/null 2>&1
+if [ "$?" = "0" ]; then
+  echo "Import initial data"
+  python manage.py loaddata initial_data/configuration_initial_data.yaml
+  python manage.py loaddata initial_data/groups_initial_data.yaml
+fi
+
+# Start server
+gunicorn updatengine.wsgi:application --bind 0.0.0.0:8000
+
+
