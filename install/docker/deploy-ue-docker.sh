53a9cb5c5fe86a1e1406d6a7329db5efbe7c3536
Noël Martinon <noel.martinon@gmail.com>
2024-12-05 08:09:47 -0400

    Add gettext-base package to apt command for the docker deployment

--- install/docker/deploy-ue-docker.sh
+++ install/docker/deploy-ue-docker.sh
@@ -2,9 +2,14 @@
 
 INST_DIR=/opt
 export GIT_BRANCH=master
+apt install git gettext-base
 cd ${INST_DIR}
 if [ ! -d "${INST_DIR}/updatengine-server" ]; then
     git clone https://github.com/updatengine-ng/updatengine-server -b ${GIT_BRANCH}
+else
+    cd "${INST_DIR}/updatengine-server"
+    git pull
+    cd -
 fi
 cp -r custom ${INST_DIR}/updatengine-server/install/docker/
 ${INST_DIR}/updatengine-server/install/docker/install.sh
