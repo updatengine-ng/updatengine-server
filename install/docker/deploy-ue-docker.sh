54ccc14ce368c93f73492d4af9aa2f447676e9b6
Noël Martinon <noel.martinon@gmail.com>
2026-01-06 09:40:07 -0400

    Fix installation for docker

--- install/docker/deploy-ue-docker.sh
+++ install/docker/deploy-ue-docker.sh
@@ -2,15 +2,16 @@
 
 ################################################
 ## UpdatEngine-server installation script with docker
-## 2026/01/03
+## 2026/01/06
 ################################################
 
 INST_DIR=/opt
 export GIT_BRANCH=$(git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/updatengine-ng/updatengine-server.git | tail --lines=1 | cut --delimiter='/' --fields=3)
 apt install git gettext-base
-cd ${INST_DIR}
 if [ ! -d "${INST_DIR}/updatengine-server" ]; then
+    cd ${INST_DIR}
     git clone https://github.com/updatengine-ng/updatengine-server -b ${GIT_BRANCH}
+    cd -
 else
     cd "${INST_DIR}/updatengine-server"
     git fetch --all
